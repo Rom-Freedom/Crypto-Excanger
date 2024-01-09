bc169df755e53d82ca2a32881d55b7ac835325d2
Rom-Freedom <romeo.dark@mail.ru>
2024-01-10 00:17:21 +0300

    create dasic structure for the app

---
+++ src/components/Input.vue
@@ -0,0 +1,24 @@
+<template>
+    <input class="input-field" type="number" placeholder="Enter a number">
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+.input-field {
+    outline: none;
+    width: 500px;
+    position: relative;
+    top: -50px;
+    border-radius: 3px;
+    border: 0;
+    padding: 10px 15px;
+    background: #fafafa;
+    color: #333;
+    font-size: 1.2em;
+}
+</style>
---
+++ src/components/Selector.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="list-currencies">
+        <li class="currency-name">Bitcoin</li>
+        <li class="currency-name" >ETH</li>
+        <li class="currency-name" >USDT</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+.list-currencies {
+    list-style: none;
+    width: 200px;
+    padding: 0;
+    border-radius: 3px;
+    border: 3px solid #000;
+}
+
+.currency-name {
+    display: block;
+    background: #1a032d;
+    color: #fff;
+    padding: 20px 0px;
+}
+.currency-name:hover, li.active {
+    background: #24043e;
+    cursor: pointer;
+}
+</style>
